69b0dea3e60652c2f5637dd917932ead76bc21a2
Dave Ames <david.john.ames@gmail.com>
2017-12-11 21:14:06 +0000

    Added a comment block describing the challenge

---
+++ challenge02.py
@@ -0,0 +1,10 @@
+######################################################################################################
+# challenge02.py by Dave Ames
+# david.john.ames@gmail.com
+# @davidames
+#
+# Problem: Challenge 02
+#
+# Add a feature that will guess the user's age then display the following message, "Hello name. Since
+# you were born in the year 2001, I think you are 16 years old"
+#######################################################################################################
