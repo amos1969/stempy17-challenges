5a51e5bf833f7e9518ed9ed5a72fcd6912f64a2f
Dave Ames <david.john.ames@gmail.com>
2017-12-11 21:14:17 +0000

    Added a comment block describing the challenge

---
+++ challenge04.py
@@ -0,0 +1,10 @@
+######################################################################################################
+# challenge04.py by Dave Ames
+# david.john.ames@gmail.com
+# @davidames
+#
+# Problem: Challenge 04
+#
+# Add a feature where it tells the user an interesting fact based on their age, eg. "Since most humans
+# sleep for about a third of their life, that means you have slept for a total of 5 years"
+#######################################################################################################
