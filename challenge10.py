af19bd950e068fb92f81f8c8b46266777eca431a
Dave Ames <david.john.ames@gmail.com>
2017-12-11 21:14:51 +0000

    Added a comment block describing the challenge

---
+++ challenge10.py
@@ -0,0 +1,10 @@
+######################################################################################################
+# challenge10.py by Dave Ames
+# david.john.ames@gmail.com
+# @davidames
+#
+# Problem: Challenge 10
+#
+#Develop a way to tell the user the name of one other previous user who shares the same birth year, eg.
+# "You were born in the same year as Michael"
+#######################################################################################################
