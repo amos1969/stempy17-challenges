1d97d1accacc9fc2d066074b59ce32bc066a9b10
Dave Ames <david.john.ames@gmail.com>
2017-12-11 21:14:39 +0000

    Added a comment block describing the challenge

---
+++ challenge08.py
@@ -0,0 +1,10 @@
+######################################################################################################
+# challenge08.py by Dave Ames
+# david.john.ames@gmail.com
+# @davidames
+#
+# Problem: Challenge 08
+#
+# Add a feature that will permanently store all data for each user (name, year, age) long after the
+# program has stopped executing or the computer has been shut down.
+#######################################################################################################
