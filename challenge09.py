beb86039f850f752d4edc40d1da3045b4cca17bb
Dave Ames <david.john.ames@gmail.com>
2017-12-11 21:14:44 +0000

    Added a comment block describing the challenge

---
+++ challenge09.py
@@ -0,0 +1,9 @@
+######################################################################################################
+# challenge09.py by Dave Ames
+# david.john.ames@gmail.com
+# @davidames
+#
+# Problem: Challenge 09
+#
+# Add a date/time stamp to the data, so that you know when a user entered the data.
+#######################################################################################################
