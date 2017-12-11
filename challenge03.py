586b53113b132833c699f91789f3303fb8b53695
Dave Ames <david.john.ames@gmail.com>
2017-12-11 21:14:12 +0000

    Added a comment block describing the challenge

---
+++ challenge03.py
@@ -0,0 +1,10 @@
+######################################################################################################
+# challenge03.py by Dave Ames
+# david.john.ames@gmail.com
+# @davidames
+#
+# Problem: Challenge 03
+#
+# Add in some data validation to check that the user is giving a valid year of birth. If they don't,
+# make the program continually ask the question until they provide a valid year.
+#######################################################################################################
