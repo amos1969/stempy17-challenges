ef6f5445b251272a4175aab34a3f1ced6ad73f77
Dave Ames <david.john.ames@gmail.com>
2017-12-11 21:14:22 +0000

    Added a comment block describing the challenge

---
+++ challenge05.py
@@ -0,0 +1,11 @@
+######################################################################################################
+# challenge05.py by Dave Ames
+# david.john.ames@gmail.com
+# @davidames
+#
+# Problem: Challenge 05
+#
+# If the user has not had their birthday yet, it's likely that the prediction about their age is 1 year
+# higher than it should be. Ask the user if the age prediction is correct, if they say no, then adjust
+# their age accordingly and display the correct age.
+#######################################################################################################
