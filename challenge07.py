ab04940c08053586086db80afbc39f88b4f41d18
Dave Ames <david.john.ames@gmail.com>
2017-12-11 21:14:33 +0000

    Added a comment block describing the challenge

---
+++ challenge07.py
@@ -0,0 +1,10 @@
+######################################################################################################
+# challenge07.py by Dave Ames
+# david.john.ames@gmail.com
+# @davidames
+#
+# Problem: Challenge 07
+#
+# If your program is being used next year, it's possible that it will be wrong. Make use of the time
+# module to ensure that your program always gives the correct calculations based on the current year.
+#######################################################################################################
