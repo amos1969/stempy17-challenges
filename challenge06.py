55e24366b6ecf9ad58fef94721554bc98c4aafff
Dave Ames <david.john.ames@gmail.com>
2017-12-11 21:14:28 +0000

    Added a comment block describing the challenge

---
+++ challenge06.py
@@ -0,0 +1,10 @@
+######################################################################################################
+# challenge06.py by Dave Ames
+# david.john.ames@gmail.com
+# @davidames
+#
+# Problem: Challenge 06
+#
+# Develop the program so that it will execute continually so that the program starts again automatically
+# after the previous user.
+#######################################################################################################
