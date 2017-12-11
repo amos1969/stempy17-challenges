e9f8b7d0aadd774d7a8a1a69caf5876f31906ce0
Dave Ames <david.john.ames@gmail.com>
2017-12-11 21:31:23 +0000

    Added user validation and lots of comments to explain what is going on

--- challenge03.py
+++ challenge03.py
@@ -8,3 +8,28 @@
 # Add in some data validation to check that the user is giving a valid year of birth. If they don't,
 # make the program continually ask the question until they provide a valid year.
 #######################################################################################################
+
+name = input("Please enter your name: ")
+# Add some user validation for the year
+year_is_valid = False
+while not year_is_valid:
+    year = input("Please enter the year you were born: ")
+    # Check if the input was actually a number
+    while not year.isnumeric():
+        # If the year wasn't a number run these two lines and check again
+        print("Please enter a valid number for the year")
+        year = input("Please enter the year you were born: ")
+    # The year must have been a number to get this far so convert it
+    year = int(year)
+    # Make sure the year is a sensible one
+    if year > 2017 or year < 1900:
+        print("How old are you again?")
+    else:
+        # If the year is valid, set this variable true to exit the loop
+        year_is_valid = True
+
+# Now we can do the calculation
+age = 2017 - year
+# The + sign is used to concatenate the name with the following string, so that the space isn't
+# added in before the .
+print("Hello", name + ". Since you were born in the year", year, "I think you are", age, "years old")
