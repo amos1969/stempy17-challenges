2b61e02088f48e7b950d47b90ce10cee4e6013ce
Dave Ames <david.john.ames@gmail.com>
2017-12-12 09:15:23 +0000

    Simple File I/O examples

---
+++ file-io/simple-files.py
@@ -0,0 +1,9 @@
+######################################################################################################
+# simple-files.py by Dave Ames
+# david.john.ames@gmail.com
+# @davidames
+#
+# Problem: Simple File Handling
+#
+# Examples of simple file handling
+#######################################################################################################
