7fdf3332f780c7a348f666ffd68273c717e817d9
Dave Ames <david.john.ames@gmail.com>
2017-12-12 09:17:05 +0000

    More complex File I/O examples

---
+++ file-io/more-complex-files.py
@@ -0,0 +1,9 @@
+######################################################################################################
+# more-complex-files.py by Dave Ames
+# david.john.ames@gmail.com
+# @davidames
+#
+# Problem: More Advanced File Handling
+#
+# Examples of more advanced file handling
+#######################################################################################################
