9bdb277ea4d23997d8fd49b10bfe28938c1e3ddc
Dave Ames <david.john.ames@gmail.com>
2017-12-13 09:51:46 +0000

    Examples of how to define functions

---
+++ functions/defining-functions.py
@@ -0,0 +1,22 @@
+###################################################################################################
+# defining-functions.py by Dave Ames
+# david.john.ames@gmail.com
+# @davidames
+#
+# Problem: Function definitions and how to call them
+#
+# Examples of how to define and call functions
+#######################################################################################################
+
+# Simple function definition
+
+
+# Function definition passing in values
+
+
+# Function definition returning values
+
+# Combining them
+
+
+
