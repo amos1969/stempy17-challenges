d6d751187e0ce0075c113b5eeb11ee092d8aebc6
Dave Ames <david.john.ames@gmail.com>
2017-12-13 15:40:06 +0000

    Example function definitions

--- functions/defining-functions.py
+++ functions/defining-functions.py
@@ -9,10 +9,17 @@
 #######################################################################################################
 
 # Simple function definition
-
+def simple():
+    print("Hello we're in the simple function")
+    print("We're still in the function here")
 
 # Function definition passing in values
-
+def welcome(name, age):
+    print("Hello", name, "welcome to the functions program")
+    print("You were", age, "years old")
+    print("Happy Birthday!")
+    age = age + 1
+    return age
 
 # Function definition returning values
 
@@ -20,4 +27,9 @@
 
 # Discuss global vs local variables
 
+the_name = input("Please enter your name: ")
+the_age = int(input("Please enter your age: "))
+the_age = welcome(the_name, the_age)
+
+print(the_age)
 
