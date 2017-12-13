8a97880d07845fb71e6a62911d2aaa3a684a4a43
Dave Ames <david.john.ames@gmail.com>
2017-12-13 09:51:17 +0000

    Code with examples of how to call functions

---
+++ functions/calling-functions.py
@@ -0,0 +1,11 @@
+###################################################################################################
+# calling-functions.py by Dave Ames
+# david.john.ames@gmail.com
+# @davidames
+#
+# Problem: Function calls
+#
+# Examples of how to call functions
+#######################################################################################################
+
+# Example function calls
