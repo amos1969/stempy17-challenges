f65eedb88d67ff921e51a5a1883ebd661ddf3bb1
Dave Ames <david.john.ames@gmail.com>
2017-12-13 15:37:40 +0000

    Function and tests for Rock Paper Scissors Game

---
+++ check_computer_won.py
@@ -0,0 +1,21 @@
+#################################################################################
+# Name:                check_if_computer_wins
+# Arguments:           computer_hand - R, P or S
+#                      user_hand - R, P or S
+# Return Value(s):     True/False - true if computer hand beats user hand
+# Description:         Compare the two hands using the usual rules for R/P/S
+#                      return true if the computer hand wins
+#################################################################################
+def check_if_computer_wins(computer_hand, user_hand):
+    computer_wins = False
+    if computer_hand == "R" and user_hand == "S":
+        computer_wins = True
+    elif computer_hand == "P" and user_hand == "R":
+        computer_wins = True
+    elif computer_hand == "S" and user_hand == "P":
+        computer_wins = True
+    return computer_wins
+
+print("Data - computer_hand = R, and user_hand = S, expected True. Actual result ->", check_if_computer_wins("R","S"))
+print("Data - computer_hand = R, and user_hand = P, expected False. Actual result ->", check_if_computer_wins("R","P"))
+print("Data - computer_hand = R, and user_hand = R, expected False. Actual result ->", check_if_computer_wins("R","R"))
