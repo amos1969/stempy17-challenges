a0a7ca5283d3915232e019c0acef4738fa4f9495
Dave Ames <david.john.ames@gmail.com>
2017-12-13 09:20:52 +0000

    Added comments to explain the code

--- file-io/more-complex-files.py
+++ file-io/more-complex-files.py
@@ -7,3 +7,22 @@
 #
 # Examples of more advanced file handling
 #######################################################################################################
+# Create an empty list
+the_text = []
+# Open the file for reading, and assign it to the variable file_handle
+file_handle = open("pi-poems.txt", "r")
+
+# Loop through the file, reading one line at a time into the line variable
+for line in file_handle:
+    # Removes non-printing characters like newlines
+    line = line.strip()
+    # Add the current line to the end of the list we created
+    the_text.append(line)
+
+# Close the file
+file_handle.close()
+
+# Pull each item out of the list, one at a time
+for item in the_text:
+    # Print each item on a single line
+    print(item)
