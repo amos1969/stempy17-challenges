6d2d2b41071461e8f89eb17dab11cc957482aadc
Dave Ames <david.john.ames@gmail.com>
2017-12-13 09:52:36 +0000

    Hangman game

---
+++ functions/hangman.py
@@ -0,0 +1,9 @@
+###################################################################################################
+# hangman.py by Dave Ames
+# david.john.ames@gmail.com
+# @davidames
+#
+# Problem: Implement a Hangman Game using functions
+#
+# The finished hangman game
+#######################################################################################################
